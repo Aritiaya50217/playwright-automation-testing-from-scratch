13c54191652029135d902745ec12aacd06318cd3
artitaya <artitaya2466@gmail.com>
2026-03-23 21:43:35 +0700

    child window

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('Browser Context Playwright test', async ({ browser }) => {
+test('@Browser Context Playwright test', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     const userName = page.locator('#username');
@@ -28,11 +28,9 @@ test('Browser Context Playwright test', async ({ browser }) => {
     console.log(allTitles);
 });
 
-test('UI Controls', async ({ page }) => {
+test('@UI Controls', async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
-    const userName = page.locator('#username');
-    const signIn = page.locator("#signInBtn");
     const dropdown = page.locator("select.form-control");
     const isClickRadio = await page.locator(".radiotextsty").last().isChecked();
     const documentLink = page.locator("[href*='documents-request']");
@@ -48,5 +46,17 @@ test('UI Controls', async ({ page }) => {
     await page.locator("#terms").uncheck();
     expect(await page.locator("#terms").isChecked()).toBeFalsy();
     await expect(documentLink).toHaveAttribute("class", "blinkingText");
+});
 
+test('@Child window hadl', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+        documentLink.click(),
+
+    ])//new page is opened
 })
